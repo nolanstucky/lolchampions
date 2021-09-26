71aa1e5f06ac21cf9e65f0f8a9d406cd9adeb0ab
Nolan Stucky <nolan@stucky.net>
2021-09-25 20:10:29 -0700

    cleaned up code and added populatechampions function and deletechampions function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var leagueChamps = ['Aatrox','Ahri','Akali','Akshan','Alistar','Amumu','Anivia','Annie','Aphelios','Ashe','AurelionSol','Azir','Bard','Blitzcrank','Brand','Braum','Caitlyn','Camille','Cassiopeia','Chogath','Corki','Darius','Diana','DrMundo','Draven','Ekko','Elise','Evelynn','Ezreal','Fiddlesticks','Fiora','Fizz','Galio','Gangplank','Garen','Gnar','Gragas','Graves','Gwen','Hecarim','Heimerdinger','Illaoi','Irelia','Ivern','Janna','JarvanIV','Jax','Jayce','Jhin','Jinx','Kaisa','Kalista','Karma','Karthus','Kassadin','Katarina','Kayle','Kayn','Kennen','Khazix','Kindred','Kled','KogMaw','Leblanc','LeeSin','Leona','Lillia','Lissandra','Lucian','Lulu','Lux','Malphite','Malzahar','Maokai','MasterYi','MissFortune','Mordekaiser','Morgana','Nami','Nasus','Nautilus','Neeko','Nidalee','Nocturne','Nunu','Olaf','Orianna','Ornn','Pantheon','Poppy','Pyke','Qiyana','Quinn','Rakan','Rammus','RekSai','Rell','Renekton','Rengar','Riven','Rumble','Ryze','Samira','Sejuani','Senna','Seraphine','Sett','Shaco','Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain','Sylas','Syndra','TahmKench','Taliyah','Talon','Taric','Teemo','Thresh','Tristana','Trundle','Tryndamere','TwistedFate','Twitch','Udyr','Urgot','Varus','Vayne','Veigar','Velkoz','Vi','Viego','Viktor','Vladimir','Volibear','Warwick','MonkeyKing','Xayah','Xerath','XinZhao','Yasuo','Yone','Yorick','Yuumi','Zac','Zed','Ziggs','Zilean','Zoe','Zyra']
+var leagueChamps = ['Aatrox','Ahri','Akali','Akshan','Alistar','Amumu','Anivia','Annie','Aphelios','Ashe','AurelionSol','Azir','Bard','Blitzcrank','Brand','Braum','Caitlyn','Camille','Cassiopeia','Chogath','Corki','Darius','Diana','DrMundo','Draven','Ekko','Elise','Evelynn','Ezreal','Fiddlesticks','Fiora','Fizz','Galio','Gangplank','Garen','Gnar','Gragas','Graves','Gwen','Hecarim','Heimerdinger','Illaoi','Irelia','Ivern','Janna','JarvanIV','Jax','Jayce','Jhin','Jinx','Kaisa','Kalista','Karma','Karthus','Kassadin','Katarina','Kayle','Kayn','Kennen','Khazix','Kindred','Kled','KogMaw','Leblanc','LeeSin','Leona','Lillia','Lissandra','Lucian','Lulu','Lux','Malphite','Malzahar','Maokai','MasterYi','MissFortune','Mordekaiser','Morgana','Nami','Nasus','Nautilus','Neeko','Nidalee','Nocturne','Nunu','Olaf','Orianna','Ornn','Pantheon','Poppy','Pyke','Qiyana','Quinn','Rakan','Rammus','RekSai','Rell','Renekton','Rengar','Riven','Rumble','Ryze','Samira','Sejuani','Senna','Seraphine','Sett','Shaco','Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain','Sylas','Syndra','TahmKench','Taliyah','Talon','Taric','Teemo','Thresh','Tristana','Trundle','Tryndamere','TwistedFate','Twitch','Udyr','Urgot','Varus','Vayne','Veigar','Velkoz','Vi','Viego','Viktor','Vladimir','Volibear','Warwick','MonkeyKing','Xayah','Xerath','XinZhao','Yasuo','Yone','Yorick','Yuumi','Zac','Zed','Ziggs','Zilean','Zoe','Zyra','Vex']
 var playedChampions = JSON.parse(localStorage.getItem("played champ"));
 var champs = [];
 
 console.log(champs)
+
+
+
+populateChampions();
+deleteChampions();
 
 function deleteChampions(){
     console.log(playedChampions)
@@ -13,15 +18,16 @@ function deleteChampions(){
         }
     }
 }
-deleteChampions();
 
-for (let i = 0; i < leagueChamps.length; i++) {
-    // $('#champ').append(`<div id='${i}'/>`)
-    $(`#champ`).append(`<img id ='${i}' style="margin: 10px;" src="http://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${leagueChamps[i]}.png">`)
-    
+function populateChampions(){
+
+    for (let i = 0; i < leagueChamps.length; i++) {
+        // $('#champ').append(`<div id='${i}'/>`)
+        $(`#champ`).append(`<img id ='${i}' style="margin: 10px;" src="http://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${leagueChamps[i]}.png">`)
+        
+    }
 }
 
-deleteChampions();
 
 
 $('#champ').on('click','img',function(){
@@ -47,4 +53,9 @@ $('#undo').click(function(){
     playedChampions.pop()
     localStorage.setItem('played champ', JSON.stringify(playedChampions));
     location.reload(true);
+})
+
+$('#random').click(function(){
+
+    $(`#randomChamp`).append(`<img style="margin: 10px;" src="http://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${leagueChamps[i]}.png">`)
 })
